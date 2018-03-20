9c789a61bbccd6b28774882390b75ad6759e185f
John Fairhurst <johnfairh@gmail.com>
2018-03-20 10:20:49 +0000

    Document side-definition

--- Sources/RubyBridgeHelpers/include/rbb_helpers.h
+++ Sources/RubyBridgeHelpers/include/rbb_helpers.h
@@ -11,10 +11,12 @@
 /* This small C module provides a speed-matching layer between Swift and the
  * Ruby API to hide some C-ish behaviour such as type-safety and longjmp()ing
  * from Swift.
- *
- * It would be part of the RubyBridge module directly but SPM does not approve.
  */
 
+/* Would ideally @import CRuby here, but causes weird problems building on
+ * Linux/Clang 6 and is impossible with the CocoaPods setup where this header
+ * file ends up in the RubyBridge module map.  This is a bit gross though.
+ */
 typedef unsigned long VALUE;
 typedef VALUE ID;
 
