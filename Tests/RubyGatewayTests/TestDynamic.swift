e55c083b7158c869a06e722f6ffa8a8d6c2ad775
James Pamplona <pamplonajames@gmail.com>
2020-10-10 16:19:06 -0700

    Add @dynamicMemberLookup and @dynamicCallable conformance.

--- Tests/RubyGatewayTests/TestDynamic.swift
+++ Tests/RubyGatewayTests/TestDynamic.swift
@@ -9,64 +9,64 @@ import XCTest
 import RubyGateway
 
 // Tests for dynamic member function
-//class skip_TestDynamic: XCTestCase {
-//
-//    /// Getter
-//    func testDynamicMemberLookupRead() {
-//        doErrorFree {
-//            try Ruby.require(filename: Helpers.fixturePath("methods.rb"))
-//
-//            guard let obj = RbObject(ofClass: "MethodsTest") else {
-//                XCTFail("Couldn't create object")
-//                return
-//            }
-//
-//            guard let strObj = obj.property else {
-//                XCTFail("Couldn't access member 'property'")
-//                return
-//            }
-//
-//            XCTAssertEqual("Default", String(strObj))
-//
-//            if let mysterious = obj.not_a_member {
-//                XCTFail("Accessed not_a_member: \(mysterious)")
-//                return
-//            }
-//        }
-//    }
-//
-//    /// Write
-//    func testDynamicMemberLookupWrite() {
-//        doErrorFree {
-//            try Ruby.require(filename: Helpers.fixturePath("methods.rb"))
-//
-//            guard let obj = RbObject(ofClass: "MethodsTest") else {
-//                XCTFail("Couldn't create object")
-//                return
-//            }
-//
-//            let newValue = "Changed it!"
-//            obj.property = RbObject(newValue)
-//
-//            guard let strObj = obj.property else {
-//                XCTFail("Couldn't access member 'property'")
-//                return
-//            }
-//
-//            XCTAssertEqual(newValue, String(strObj))
-//
-//            RbError.history.clear()
-//            obj.bad_property = RbObject(23)
-//            XCTAssertNotNil(RbError.history.mostRecent)
-//
-//            RbError.history.clear()
-//            obj.property = nil
-//            XCTAssertEqual(.nilObject, obj.property)
-//        }
-//    }
-//
-//    static var allTests = [
-//        ("testDynamicMemberLookupRead", testDynamicMemberLookupRead),
-//        ("testDynamicMemberLookupWrite", testDynamicMemberLookupWrite),
-//    ]
-//}
+class TestDynamic: XCTestCase {
+
+    /// Getter
+    func testDynamicMemberLookupRead() {
+        doErrorFree {
+            try Ruby.require(filename: Helpers.fixturePath("methods.rb"))
+
+            guard let obj = RbObject(ofClass: "MethodsTest") else {
+                XCTFail("Couldn't create object")
+                return
+            }
+
+            guard let strObj = obj.property else {
+                XCTFail("Couldn't access member 'property'")
+                return
+            }
+
+            XCTAssertEqual("Default", String(strObj))
+
+            if let mysterious = obj.not_a_member {
+                XCTFail("Accessed not_a_member: \(mysterious)")
+                return
+            }
+        }
+    }
+
+    /// Write
+    func testDynamicMemberLookupWrite() {
+        doErrorFree {
+            try Ruby.require(filename: Helpers.fixturePath("methods.rb"))
+
+            guard let obj = RbObject(ofClass: "MethodsTest") else {
+                XCTFail("Couldn't create object")
+                return
+            }
+
+            let newValue = "Changed it!"
+            obj.property = RbObject(newValue)
+
+            guard let strObj = obj.property else {
+                XCTFail("Couldn't access member 'property'")
+                return
+            }
+
+            XCTAssertEqual(newValue, String(strObj))
+
+            RbError.history.clear()
+            obj.bad_property = RbObject(23)
+            XCTAssertNotNil(RbError.history.mostRecent)
+
+            RbError.history.clear()
+            obj.property = nil
+            XCTAssertEqual(.nilObject, obj.property)
+        }
+    }
+
+    static var allTests = [
+        ("testDynamicMemberLookupRead", testDynamicMemberLookupRead),
+        ("testDynamicMemberLookupWrite", testDynamicMemberLookupWrite),
+    ]
+}
